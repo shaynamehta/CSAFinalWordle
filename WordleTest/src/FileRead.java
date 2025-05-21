1bbc5a8c1e026311cef798fe804b4cbba40a2b86
1909873 <1909873@01-18113.pusd.dom>
2025-05-21 13:36:44 -0700

    starting to setup classes - not done yet

--- WordleTest/src/FileRead.java
+++ WordleTest/src/FileRead.java
@@ -1,4 +1,19 @@
 
+/*
+ * ok so we have to use inheritance in the project so i'm thinking we can use it here since it's the easiest 
+ * implementation 
+ * we can do like an overall word bank class and then have the themes be "theme" extends WordBank so that
+ * if the user selects no theme it just pulls from the main word bank and if they choose a theme it goes to the extended
+ * class
+ * 
+ * its kinda useless but for now that's what i think is the best way to implement inheritance
+ * 
+ * also in this class we will turn the answer into a String array
+ * 
+ */
+
 public class FileRead {
 
+	
+	
 }
